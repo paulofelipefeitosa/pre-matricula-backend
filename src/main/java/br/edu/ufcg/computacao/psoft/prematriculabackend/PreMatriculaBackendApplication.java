0b64d5b562cd8713c7d77917c0442d7a82a758e4
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-22 19:42:42 -0300

    Setup spring boot environment

---
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/PreMatriculaBackendApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ufcg.computacao.psoft.prematriculabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreMatriculaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreMatriculaBackendApplication.class, args);
+	}
+}
