c3aa7dd07f695427eb991b0be346e2b562c49289
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-07-24 16:53:26 -0300

    Add OAuth2 security authentication

--- src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/PreMatriculaBackendApplication.java
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/PreMatriculaBackendApplication.java
@@ -2,8 +2,10 @@ package br.edu.ufcg.computacao.psoft.prematriculabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class PreMatriculaBackendApplication {
 
 	public static void main(String[] args) {
