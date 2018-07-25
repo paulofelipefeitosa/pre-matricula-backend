67551581af36c584348f039b4708f05fb559a283
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-24 21:45:41 -0300

    Add users controller

---
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/controllers/UserController.java
@@ -0,0 +1,16 @@
+package br.edu.ufcg.computacao.psoft.prematriculabackend.controllers;
+
+import java.security.Principal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    
+    @GetMapping
+    public Principal user(Principal principal) {
+        return principal;
+    }
+}
