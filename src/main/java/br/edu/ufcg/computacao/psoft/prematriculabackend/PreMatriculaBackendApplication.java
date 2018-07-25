22ac76a2f00e254d27da6995f109604ed734b5ba
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-24 21:43:56 -0300

    Add OAuth2 client application

--- src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/PreMatriculaBackendApplication.java
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/PreMatriculaBackendApplication.java
@@ -1,14 +1,41 @@
 package br.edu.ufcg.computacao.psoft.prematriculabackend;
 
+import javax.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
-@EnableOAuth2Sso
-public class PreMatriculaBackendApplication {
+@EnableOAuth2Client
+public class PreMatriculaBackendApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PreMatriculaBackendApplication.class, args);
-	}
+    @Autowired
+    OAuth2ClientContext oauth2ClientContext;
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PreMatriculaBackendApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+            .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll()
+            .anyRequest()
+                .authenticated()
+            .and().logout().logoutSuccessUrl("/").permitAll()
+            .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+    }
+
+    private Filter ssoFilter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
