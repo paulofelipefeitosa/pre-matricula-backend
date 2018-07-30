9f3e976e72a2acb64e0259d4fc77f7e8d8dac444
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-30 02:42:20 -0300

    Return authenticated user by google principal extractor

--- src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/controllers/UserController.java
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/controllers/UserController.java
@@ -1,6 +1,6 @@
 package br.edu.ufcg.computacao.psoft.prematriculabackend.controllers;
 
-import java.security.Principal;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,8 +10,8 @@ public class UserController {
 
     @RequestMapping("/users")
     @ResponseBody
-    public Principal user(Principal principal) {
-        return principal;
+    public Object user(Authentication authentication) {
+        return authentication.getPrincipal();
     }
     
 }
