a47d974d0fc34cc4afca1bcd68c2e23e8cc4d6b0
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-30 02:41:35 -0300

    Remove security application specifications

--- src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/PreMatriculaBackendApplication.java
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/PreMatriculaBackendApplication.java
@@ -1,25 +1,16 @@
 package br.edu.ufcg.computacao.psoft.prematriculabackend;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestOperations;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Controller;
 
@@ -27,27 +18,6 @@ import org.springframework.stereotype.Controller;
 @EnableOAuth2Sso
 @Controller
 public class PreMatriculaBackendApplication extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails resource,
-            OAuth2ClientContext context) {
-        return new OAuth2RestTemplate(resource, context);
-    }
-
-    @Bean
-    public AuthoritiesExtractor authoritiesExtractor(OAuth2RestOperations template) {
-        return map -> {
-            ArrayList<HashMap<String, String>> emailProperty =
-                    (ArrayList<HashMap<String, String>>) map.get("emails");
-            HashMap<String, String> emailValue = emailProperty.get(0);
-            String email = emailValue.get("value");
-            if (email.contains("@ccc.ufcg.edu.br")) {
-                return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
-            } else {
-                throw new BadCredentialsException("User does not belong to CCC organization.");
-            }
-        };
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
