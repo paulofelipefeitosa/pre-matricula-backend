7bb4128e74b22d28b39f602c3d6d093dc273724e
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-30 02:44:56 -0300

    Update Coordinator to implement security oauth2 details

--- src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/models/user/coordinator/Coordinator.java
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/models/user/coordinator/Coordinator.java
@@ -1,12 +1,25 @@
 package br.edu.ufcg.computacao.psoft.prematriculabackend.models.user.coordinator;
 
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import br.edu.ufcg.computacao.psoft.prematriculabackend.models.user.Role;
 import br.edu.ufcg.computacao.psoft.prematriculabackend.models.user.User;
 
 public class Coordinator extends User {
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
     public Coordinator(String email, String name, String enrollmentNumber) {
-        super(email, name, enrollmentNumber, Role.COORDINATOR);
+        super(name, email, enrollmentNumber, Role.COORDINATOR);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return AuthorityUtils.commaSeparatedStringToAuthorityList(super.getRole().toString());
     }
 
 }
