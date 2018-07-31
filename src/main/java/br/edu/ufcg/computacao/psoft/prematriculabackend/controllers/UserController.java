ede7173eb3c27a752dc0f4b3a82260180bbc1e15
David Ferreira <david.quaresma@ccc.ufcg.edu.br>
2018-07-31 13:51:39 -0300

    Add cross origin.

--- src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/controllers/UserController.java
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/controllers/UserController.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(value = "/api")
+@CrossOrigin(value = "*")
 public class UserController {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
