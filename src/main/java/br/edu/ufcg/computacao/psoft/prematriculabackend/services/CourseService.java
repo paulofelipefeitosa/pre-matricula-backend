bac7e4f65f75f49e6356639c09d02a31b4aa0a4f
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-31 02:45:33 -0300

    Add initial implementation of CourseService

--- src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/services/CourseService.java
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/services/CourseService.java
@@ -4,30 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import br.edu.ufcg.computacao.psoft.prematriculabackend.models.course.Course;
-import br.edu.ufcg.computacao.psoft.prematriculabackend.models.user.User;
+import br.edu.ufcg.computacao.psoft.prematriculabackend.models.exceptions.NoSuchElementException;
 
 @Service
 public class CourseService {
 
     private List<Course> courseList = new ArrayList<Course>();
 
+    public List<Course> getAll() {
+        return this.courseList;
+    }
+
     public Course create(Course course) {
         this.courseList.add(course);
         return this.courseList.get(this.courseList.size() - 1);
     }
 
-    public Course getCourseByCode(String code) {
+    public Course getByCode(String code) {
         for (Course course : this.courseList) {
             if (course.getCode().equals(code)) {
                 return course;
             }
         }
-        return null;
+        throw new NoSuchElementException("There is not course with code [" + code + "]");
     }
 
-    public void delete(User user) {
-        if (this.courseList.contains(user)) {
-            this.courseList.remove(user);
-        }
+    public void delete(String code) {
+        Course course = this.getByCode(code);
+        this.courseList.remove(course);
+    }
+
+    public void deleteAll() {
+        this.courseList = new ArrayList<Course>();
     }
 }
