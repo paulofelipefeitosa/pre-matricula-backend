db741288a3d795ce92070fa4c0ea210fd2b78054
Paulo Felipe <paulofelipe.feitosa@gmail.com>
2018-07-31 00:54:37 -0300

    User rest controller refactor

--- src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/controllers/UserController.java
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/controllers/UserController.java
@@ -3,15 +3,18 @@ package br.edu.ufcg.computacao.psoft.prematriculabackend.controllers;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(value = UserController.USERS_ENDPOINT)
 public class UserController {
 
-    @RequestMapping("/users")
-    @ResponseBody
-    public Object user(Authentication authentication) {
+    public static final String USERS_ENDPOINT = "/users";
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody Object user(Authentication authentication) {
         return authentication.getPrincipal();
     }
-    
+
 }
