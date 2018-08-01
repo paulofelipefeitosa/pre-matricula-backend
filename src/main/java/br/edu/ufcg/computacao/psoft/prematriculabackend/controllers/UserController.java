cbf30a11471b4811bb2df7be5c0660bc12bf40b9
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2018-08-01 17:32:01 -0300

    Unauthorizing coordinator at user api

--- src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/controllers/UserController.java
+++ src/main/java/br/edu/ufcg/computacao/psoft/prematriculabackend/controllers/UserController.java
@@ -25,7 +25,7 @@ import br.edu.ufcg.computacao.psoft.prematriculabackend.services.UserService;
 public class UserController {
 
 	private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
-	
+
 	@Autowired
 	private AuthenticationService authService;
 
@@ -43,20 +43,20 @@ public class UserController {
 			@RequestHeader(required = true, value = AuthenticationController.TOKEN_VALUE_HEADER_KEY) String tokenValue) {
 		logger.info(user.toString());
 		User authUser = this.userService.getUserByEmail(user.getEmail());
-		logger.info(authUser.toString());
-		if(authUser.getEmail().equals(user.getEmail())) {
+		if (authUser != null && authUser.getEmail().equals(user.getEmail())) {
+			logger.info(authUser.toString());
 			this.authService.createToken(tokenValue, user);
 			this.userService.save(user);
 			return user;
 		}
 		throw new UnauthorizedException("Unauthorized operation");
 	}
-	
+
 	@RequestMapping(value = "/coordinator", method = RequestMethod.POST)
 	public @ResponseBody Coordinator createUser(@RequestBody Coordinator user,
 			@RequestHeader(required = true, value = AuthenticationController.TOKEN_VALUE_HEADER_KEY) String tokenValue) {
 		User authUser = this.userService.getUserByEmail(user.getEmail());
-		if(authUser.getRole().equals(Role.COORDINATOR)) {
+		if (authUser.getRole().equals(Role.COORDINATOR)) {
 			this.authService.createToken(tokenValue, user);
 			return user;
 		}
